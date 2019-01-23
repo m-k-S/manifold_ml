e257d1a0c51608fec75467884596252d94c3df74
m <maxaalto@gmail.com>
2019-01-23 00:27:32 -0500

    updating readme

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# Manifold Metric Learning
-
-##
+Manifold Metric Learning
+=====
 
 ## Dependencies
 
@@ -9,3 +8,39 @@ Our code was written and tested using Python 3.6.7; other versions may work but 
 - scipy 1.2.0
 - scikit-learn 0.20.2
 - networkx 2.2 [only for MDS]
+
+The code should run out of the box, assuming dependencies are installed. Please open an issue if you encounter any errors in setup.
+
+## Usage Guide
+
+#### Metric Learning Performance Tests
+
+To run clustering tests on metric learning, use the following syntax:
+
+```
+python3 metric_learning.py --dataset DATASET --lmbd LAMBDA --reg REG --clus```
+
+Similarly, for classification, use the following:
+```
+python3 metric_learning.py --dataset DATASET --K k --lmbd LAMBDA --reg REG --clf```
+
+k is used in classification tests to specify the k in k-nearest neighbor. Reg should be a float between 0.0 and 1.0 and specifies the regularization term in the loss function. Lambda should be a float, and specifies how much scaling is penalized during optimization. Dataset names available out of the box are: football, polbooks, karate, adjnoun, helicoid and 20newsgroup.
+
+#### Metric Learning
+
+To perform manifold metric learning on your dataset of choice, import one of the loss functions (`mmc_loss_generic` or `lmnn_loss_generic`) and run the following optimization (for example):
+
+```
+Q0 = numpy.diag([1 for _ in range(dim)])
+res = scipy.optimize.minimize(lmnn_loss_generic, Q0, args=(None, K, reg, lmbd, fxn_mfd, fxn_mfd_dist, fxn_integrand, data, labels), method='Powell')
+```
+
+`Q0` should be the initial linear transformation; in our work we have generally set it to the identity matrix. `fxn_mfd` is a function that takes points in the base Euclidean space and maps them to the manifold (as described in the paper). `fxn_mfd_dist` is the distance function on that manifold; if no explicit distance function exists, an approximation function can be created using `learn_manifold_distance.py` (and the appropriate `fxn_integrand` must be set). `data` should be an array of points in the base space, and `labels` should be a corresponding array of point labels.
+
+#### Manifold Distance Approximation
+
+To approximate the distance function on a manifold, one must compute the arc length integrand as a function of paths (as described in the paper). Plugging this integrand into the function `learn_distance` in `learn_manifold_distance.py` will yield an approximation of the true manifold distance (see paper for empirical rate of convergence)
+
+### MDS
+
+Minimizing the MDS loss function in `generalized_mds.py` requires the functions `fxn_mfd`, `fxn_mfd_dist`, and (optionally) `fxn_integrand`, as described above. It also requires a nxn distance matrix D (where n is the number of points to be embedded, and D<sub>ij</sub> is the distance between the ith and jth points), an initialized matrix of embedded points (which may be generated randomly), and the desired embedding dimension.
--- metric_learning.py
+++ metric_learning.py
@@ -7,11 +7,16 @@ import scipy.io
 #
 # ----------------------------------------------------------------------------------------------------
 
+def ensure_dir(path):
+    if not os.path.exists(path):
+       os.makedirs(path)
+
 if __name__ == "__main__":
 
     from cluster_tests import *
     from classification_tests import *
     from manifold_functions import *
+    import os
     import argparse
 
     parser = argparse.ArgumentParser()
@@ -23,12 +28,12 @@ if __name__ == "__main__":
     parser.add_argument('--clus', action='store_true')
     args = parser.parse_args()
 
-    k = int(args.K)
     reg = float(args.reg)
     lmbd = float(args.lmbd)
     datasetname = args.dataset
 
     if datasetname == 'karate':
+        ensure_dir(os.path.dirname(os.path.abspath(__file__)) + "/karate")
         Beuc   = scipy.io.loadmat('./data/karate.mat')['Beuc']
         Bhyp   = scipy.io.loadmat('./data/karate.mat')['Bhyp']
         Labels = scipy.io.loadmat('./data/karate.mat')['Labels']
@@ -38,6 +43,7 @@ if __name__ == "__main__":
         fxn_integrand = None
 
     elif datasetname == 'helicoid':
+        ensure_dir(os.path.dirname(os.path.abspath(__file__)) + "/helicoid")
         Beuc = scipy.io.loadmat('./data/helicoid.mat')['data']  # 3D
         Bhyp   = scipy.io.loadmat('./data/helicoid.mat')['base'] # 2D
         Labels = scipy.io.loadmat('./data/helicoid.mat')['labels']
@@ -47,6 +53,7 @@ if __name__ == "__main__":
         fxn_integrand = integrand_helicoid
 
     elif datasetname == 'football':
+        ensure_dir(os.path.dirname(os.path.abspath(__file__)) + "/football")
         Beuc   = scipy.io.loadmat('./data/football.mat')['Beuc']
         Bhyp   = scipy.io.loadmat('./data/football.mat')['Bhyp']
         Labels = scipy.io.loadmat('./data/football.mat')['Labels']
@@ -56,6 +63,7 @@ if __name__ == "__main__":
         fxn_integrand = None
 
     elif datasetname == 'polbooks':
+        ensure_dir(os.path.dirname(os.path.abspath(__file__)) + "/polbooks")
         Beuc   = scipy.io.loadmat('./data/polbooks.mat')['Beuc']
         Bhyp   = scipy.io.loadmat('./data/polbooks.mat')['Bhyp']
         Labels = scipy.io.loadmat('./data/polbooks.mat')['Labels']
@@ -65,6 +73,7 @@ if __name__ == "__main__":
         fxn_integrand = None
 
     elif datasetname == 'adjnoun':
+        ensure_dir(os.path.dirname(os.path.abspath(__file__)) + "/adjnoun")
         Beuc = scipy.io.loadmat('./data/adjnoun.mat')['Beuc']
         Bhyp = scipy.io.loadmat('./data/adjnoun.mat')['Bhyp']
         Labels = scipy.io.loadmat('./data/adjnoun.mat')['Labels']
@@ -74,6 +83,7 @@ if __name__ == "__main__":
         fxn_integrand = None
 
     elif datasetname == '20newsgroup':
+        ensure_dir(os.path.dirname(os.path.abspath(__file__)) + "/20newsgroup")
         Beuc = scipy.io.loadmat('./data/20newsgroup.mat')['Beuc']
         Bhyp = scipy.io.loadmat('./data/20newsgroup.mat')['Bhyp']
         Labels = scipy.io.loadmat('./data/20newsgroup.mat')['Labels']
@@ -91,6 +101,7 @@ if __name__ == "__main__":
     nrounds = 10
 
     if args.clf:
+        k = int(args.K)
         err_euc_orig, err_euc_qlrn, err_mfd_orig, err_mfd_qlrn = do_classification_tests_all(nrounds, train_ratio, k, reg, lmbd, Beuc, fxn_euc, fxn_euc_dist, Bhyp, fxn_mfd, fxn_mfd_dist, fxn_integrand, Labels)
         scipy.io.savemat('./'+datasetname+'/'+datasetname+'_CLF_err_euc_orig_reg'+str(reg)+'_k'+str(k)+'_lmbd'+str(lmbd)+'.mat', mdict = {'arr': err_euc_orig})
         scipy.io.savemat('./'+datasetname+'/'+datasetname+'_CLF_err_euc_qlrn_reg'+str(reg)+'_k'+str(k)+'_lmbd'+str(lmbd)+'.mat', mdict = {'arr': err_euc_qlrn})
@@ -98,6 +109,7 @@ if __name__ == "__main__":
         scipy.io.savemat('./'+datasetname+'/'+datasetname+'_CLF_err_mfd_qlrn_reg'+str(reg)+'_k'+str(k)+'_lmbd'+str(lmbd)+'.mat', mdict = {'arr': err_mfd_qlrn})
 
     elif args.clus:
+        k = 0
         err_euc_orig, err_euc_qlrn, err_mfd_orig, err_mfd_qlrn = do_cluster_tests_all(nrounds, train_ratio, k, reg, lmbd, Beuc, fxn_euc, fxn_euc_dist, Bhyp, fxn_mfd, fxn_mfd_dist, fxn_integrand, Labels)
         scipy.io.savemat('./'+datasetname+'/'+datasetname+'_CLUS_err_euc_orig_reg'+str(reg)+'_lmbd'+str(lmbd)+'.mat', mdict = {'arr': err_euc_orig})
         scipy.io.savemat('./'+datasetname+'/'+datasetname+'_CLUS_err_euc_qlrn_reg'+str(reg)+'_lmbd'+str(lmbd)+'.mat', mdict = {'arr': err_euc_qlrn})
